7628036d1683a6cdc26bf9289129e1ae437dd7fa
carlosdavila <carlosdavila@tecnicoecuador.edu.ec>
2022-01-28 13:42:05 -0500

    Refactorizar codigo

--- Katas/Katas/NumerosRomanos.cs
+++ Katas/Katas/NumerosRomanos.cs
@@ -8,26 +8,25 @@ namespace Katas
         {
             var resultado = new StringBuilder();
             var restante = numeroAConvertir;
-            if (restante >= 9)
-            {
-                resultado.Append("IX");
-                restante -= 9;
-            }
-            if (restante >= 5)
-            {
-                resultado.Append("V");
-                restante -= 5;
-            }
-            if (restante >= 4)
-            {
-                resultado.Append("IV");
-                restante -= 4;
-            }
+            restante = agregarNumerosRomanos(restante, 9, "IX", resultado);
+            restante = agregarNumerosRomanos(restante, 5, "V", resultado);
+            restante = agregarNumerosRomanos(restante, 4, "IV", resultado);
             for (int i = 0; i < restante; i++)
             {
                 resultado = resultado.Append("I");
             }
             return resultado.ToString();
+        }
+
+        private static int agregarNumerosRomanos(int arabico, int valorNumeroRomano, string digitosRomanos, StringBuilder stringBuilder)
+        {
+            int resultado = arabico;
+            if(resultado >= valorNumeroRomano)
+            {
+                stringBuilder.Append(digitosRomanos);
+                resultado -= valorNumeroRomano;
+            }
+            return resultado;
         }
     }
 }
